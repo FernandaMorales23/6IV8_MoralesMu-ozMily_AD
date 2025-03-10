eac93f842d10ddea83c03d9cae7439ab9014b523
Fernanda Morales <mmoralesm2206@alumno.ipn.mx>
2025-03-10 15:36:46 -0600

    02.Diccionario

---
+++ ElementosBasicosEstadistica/02.Diccionario.py
@@ -0,0 +1,12 @@
+import pandas as pd 
+
+##Escribir una funcion que reciba un diccionario con las notas de los estudiantes del curso y devuelve una serie con minimo, máximo, media y desviación tipica.
+
+def estadistica_notas(notas):
+    notas = pd.Series(notas)
+    estadisticas = pd.Series([notas.min(), notas.max(), notas.mean(), notas.std()], index=['Min','Max','Media','Desviacion estándar'])
+    return estadisticas
+
+notas = {'Juan': 9, 'Juanita': 7, 'Pedro': 6.6, 'Fabián':8.5, 'Maximiliano':7.5, 'Sandra': 9.8, 'Rosario':9}
+
+print(estadistica_notas(notas))
