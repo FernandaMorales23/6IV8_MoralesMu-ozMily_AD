de2669c81e4dd2c0ed63f55208df1db7a4991025
Fernanda Morales <mmoralesm2206@alumno.ipn.mx>
2025-03-10 15:44:24 -0600

    Aprobados

--- ElementosBasicosEstadistica/02.Diccionario.py
+++ ElementosBasicosEstadistica/02.Diccionario.py
@@ -7,6 +7,11 @@ def estadistica_notas(notas):
     estadisticas = pd.Series([notas.min(), notas.max(), notas.mean(), notas.std()], index=['Min','Max','Media','Desviacion estándar'])
     return estadisticas
 
+def aprobados(notas):
+    notas = pd.Series(notas)
+    return notas[notas >= 6].sort_values(ascending = False)
+
 notas = {'Juan': 9, 'Juanita': 7, 'Pedro': 6.6, 'Fabián':8.5, 'Maximiliano':7.5, 'Sandra': 9.8, 'Rosario':9}
 
 print(estadistica_notas(notas))
+print(aprobados(notas))
